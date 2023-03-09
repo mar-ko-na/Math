20e6cb538a2a78c3c3e2d0ceb85049ac20c6a920
markona <m_kokoreva@list.ru>
2023-03-09 21:19:41 +0500

    TaskItemActivity, какая-то какаха

---
+++ app/src/main/java/com/example/math/TaskItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.math
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_item)
+    }
+}
